7f21cc74b431894f683abf86f13bdfe0fe605f42
justinmmorgan21 <justinmorgan21@gmail.com>
2024-10-20 17:50:13 -0500

    added controller with news api get request, encrypted api key

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,6 @@
 class ArticlesController < ApplicationController
+  def index
+    response = HTTP.get("https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=#{Rails.application.credentials[:news_api_key]}")
+    render json: response.parse(:json)
+  end
 end
